988b5dc1ac9130d063c1fc3e4d61c89be2181b0e
Joshua Jansen <joshua@itflows.nl>
2012-09-25 13:32:42 +0200

    Initial Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shopscore::Application.config.secret_token = '6aa57c1acbd64f9a7d0b9c61f065ee2305880a01956101fc46762e3a8a4c54f0da50dafb1f79ad9f4576d7615161d8e11a9dfcccd6d13a2af27994c70d57a481'
